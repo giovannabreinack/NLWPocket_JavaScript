155b005dd1bcbdc59ab634863c8e9a0c3fb00b94
Gio <105086221+giovannabreinack@users.noreply.github.com>
2024-09-12 14:22:21 -0300

    adicionando o menu do app

--- index.js
+++ index.js
@@ -1,4 +1,42 @@
-function start() {
-   // Menu da aplicação 
+const { select } = require('@inquirer/prompts')
+
+const start = async () => {
+
+   while(true){
+
+      const opcao = await select({
+         message: "Menu >",
+         choices: [
+            {
+               name: "Cadastrar Metas",
+               value: "cadastrar"
+            },
+            {
+               name: "Listar Metas",
+               value: "listar"
+            },
+            {
+               name: "Sair",
+               value: "sair"
+            }
+         ]
+      })
+
+      switch(opcao) {
+         case "cadastrar":
+            console.log("Vamos Cadastrar")
+            break
+
+            case "listar":
+               console.log("Vamos Listar")
+               break
+
+               case "sair":
+                  console.log("Até a próxima!")
+                  return
+      }
+   }
 
 }
+
+start()
