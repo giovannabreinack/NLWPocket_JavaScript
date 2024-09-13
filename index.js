08a6ca968f535a50e0b0de1d9f2087d280e56011
Gio <105086221+giovannabreinack@users.noreply.github.com>
2024-09-13 11:33:17 -0300

    Correção de bug

--- index.js
+++ index.js
@@ -30,14 +30,14 @@ const cadastrarMeta = async () => {
       instructions: false,
    })
 
+   metas.forEach((m) => {
+      m.checked = false
+   })
+
    if(respostas.length == 0){
       console.log("Nenhuma meta foi selecionada")
       return
    }
-
-   metas.forEach((m) => {
-      m.checked = false
-   })
 
    respostas.forEach((resposta) => {
       const meta = metas.find((m) => {
